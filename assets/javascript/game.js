06621def9fa737ece4494fb6fd543932c43cfe3f
unknown <cateran1995@gmail.com>
2019-05-18 20:31:02 -0700

    <Psuedo Code and Basic HTML Done>

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+//Psuedo Code
+
+//Create A Random Letter Generator.
+
+//Ask User For Guess.
+
+//Check If Guess Is Correct.
+
+//If Guess Is Correct, User Wins.
+
+//If Guess Is Incorrect, User Loses. 
