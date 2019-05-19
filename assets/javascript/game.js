9d773a7f714d631c9ef27465227f13179e1ebb23
unknown <cateran1995@gmail.com>
2019-05-18 21:25:50 -0700

    <FINALLY FIGURED OUT RANDOM LETTER GENERATOR>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,16 @@
+var won = 0;
+var lost = 0;
+var attempts = 10;
+var usedArray = [];
+var randomLetter = randomLetter;
+var letters = "qwertyuiopasdfghjklzxcvbnm"
+
+randomLetter = letters[Math.floor(Math.random() * letters.length)];
+
+console.log(randomLetter);
+
+
+
 //Psuedo Code
 
 //Create A Random Letter Generator.
@@ -8,4 +21,4 @@
 
 //If Guess Is Correct, User Wins.
 
-//If Guess Is Incorrect, User Loses. 
+//If Guess Is Incorrect, User Loses.
