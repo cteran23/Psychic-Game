b3a19969fa84671bc3aeb69fc72cb13f91cd1d5f
unknown <cateran1995@gmail.com>
2019-05-18 22:50:18 -0700

    <Added All Basic Function>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,51 @@
-var won = 0;
-var lost = 0;
-var attempts = 10;
-var usedArray = [];
 var randomLetter = randomLetter;
 var letters = "qwertyuiopasdfghjklzxcvbnm"
+var win = 0;
+var loss = 0;
+var score = 0;
+var guesses = 10;
+var usedArray = [];
 
 randomLetter = letters[Math.floor(Math.random() * letters.length)];
-
 console.log(randomLetter);
 
+function computerGuess() {
+    randomLetter = letters[Math.floor(Math.random() * letters.length)];
+    console.log(randomLetter);
+}
 
+document.onkeyup = function (event) {
+    var userGuess = event.key;
+
+    if (userGuess === randomLetter) {
+        win++;
+        attempts = 10;
+        usedArray = [];
+    }
+
+    computerGuess();
+    if (userGuess !== randomLetter) {
+        guesses--;
+    }
+
+    if (guesses == 0) {
+        loss++;
+        usedArray = []
+        guesses = 10;
+    }
+
+    if (usedArray.indexOf(userGuess) >= 0) {
+    }
+    else {
+        usedArray.push(userGuess);
+        document.getElementById("guessesleft").innerHTML = usedArray;
+        console.log(usedArray);
+    }
+
+    document.getElementById("win").innerHTML = win;
+    document.getElementById("loss").innerHTML = loss;
+    document.getElementById("guesses").innerHTML = guesses;
+}
 
 //Psuedo Code
 
